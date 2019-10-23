e3830473a85a3ac627f7cd2c1b5244e1b17c8fde
Austin Coulson <22.austin.coulson@ksd.org>
2019-10-23 12:01:02 -0700

    foo

---
+++ Asessement/Assets/Scripts/Assesment script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assesmentscript : MonoBehaviour
+{
+    float timer = 0;
+    public float onTime = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 0 && timer < onTime)
+            Destroy(gameObject);
+    }
+}
---
+++ Asessement/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyHealth : MonoBehaviour
+{
+  public int enemyhealth = 10;
+    public GameObject prefab;
+    private void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if (collision.gameObject.tag == "Bullet")
+        {
+            enemyhealth--;
+
+
+
+
+
+
+        }
+        if (enemyhealth < 1)//health < 1
+        {
+            Destroy(gameObject);
+            GameObject coin = Instantiate(prefab, transform.position, Quaternion.identity);
+
+
+
+        }
+    }
+}
+
